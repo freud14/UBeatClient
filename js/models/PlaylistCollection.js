9c402d7bbc65c13a9772daf2018aab2a0becfc2a
Chamlee <arnaud8ruckebusch@gmail.com>
2014-10-27 12:51:38 -0400

    bit improvment

--- js/models/PlaylistCollection.js
+++ js/models/PlaylistCollection.js
@@ -9,6 +9,18 @@ define([
   PlaylistCollection = Backbone.Collection.extend({
     url: config.API_URL + 'playlists/',
     model : Playlist,
+
+    initialize: function(options) {
+      
+    },
+    parse: function(response) {
+      return response.traks;
+    },
+
+    fetch: function(options) {
+      options = _.defaults((options || {}), {url: config.API_URL + 'unsecure/playlists/'});
+      return Backbone.Collection.prototype.fetch.call(this, options);
+    },
   });
 
   return PlaylistCollection;
--- js/views/playlist.js
+++ js/views/playlist.js
@@ -4,14 +4,16 @@ define([
   'backbone',
   'models/Playlist',
   'models/PlaylistCollection',
+  'views/playlist',
   'text!templates/playlist.html',
-], function($, _, Backbone, Playlist, PlaylistCollection, playlistTemplate){
+], function($, _, Backbone, Playlist, PlaylistCollection, PlaylistView, playlistTemplate){
   var PlaylistView = Backbone.View.extend({
     el: $('#page-wrapper'),
     initialize: function(options) {
       _.bindAll(this, 'render');
 
       var self = this;
+
       this.trackEventBus = _.extend({}, Backbone.Events);
       this.trackEventBus.bind("addPlaylist", this.addPlaylist, this);
       this.trackEventBus.bind("removePlaylist", this.removePlaylist, this);
@@ -21,32 +23,38 @@ define([
       this.playlistModel = new Playlist();
       this.playlistModel.bind('change add remove sync', this.render, this);
       this.playlistModel.fetch();
+
+      this.playlistCollection = new PlaylistCollection();
+      this.playlistCollection.bind('change add remove sync', this.render, this);
+      this.playlistCollection.fetch();
     },
     render: function() {
       var self = this;
 
-      var data = {album : this.playlistModel.toJSON()};
+      var data = {playlist : this.playlistCollection.toJSON()}; // pas bon
       var compiledTemplate = _.template( playlistTemplate, data );
       this.$el.html( compiledTemplate );
+      console.log(data);
 
-      /*this.$el.find('.album-playlistchoice').html(new PlaylistChoiceView({
+      /*this.$el.find('.playlists-list').html(new PlaylistChoiceView({
         collection: this.playlistCollection,
         eventBus : this.playlistChoiceEventBus,
-      }).render().el);
+      }).render().el);*/
 
-      var trackList = this.$el.find('#track-list');
-      trackList.empty();
-      this.trackCollection.each(function(track, index, context) {
-        trackList.append(new TrackView({
-          model: track,
+      var playlistTable = this.$el.find('#playlists-table');
+      playlistTable.empty();
+      this.playlistCollection.each(function(playlist, index, context) {
+        playlistTable.append(new PlaylistView({
+          model: playlist,
           playlistCollection: self.playlistCollection,
           eventBus : self.trackEventBus
         }).render().el);
-      });*/
+      });
 
       return this;
     },
     addPlaylist: function(playlist) {
+      
       //playlist.save();
     },
     removePlaylist: function(playlist) {
