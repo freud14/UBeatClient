8b5983b264d115394847fc474c0d82044c1acfd9
Henri Wallart <henri.wallart@gmail.com>
2014-12-01 19:46:31 -0500

    Playlists of a user in its user page

--- js/views/user.js
+++ js/views/user.js
@@ -18,11 +18,19 @@ define([
       this.userModel.fetch({id : options.id}).done(function() {
 	      self.render();
       });
+      
+      this.playlistModel = new Playlist();
+      this.playlistModel.bind('change add destroy sync', this.render, this);
+      this.playlistModel.fetch();
+
+      this.playlistCollection = new PlaylistCollection();
+      this.playlistCollection.bind('change add remove sync', this.render, this);
+      this.playlistCollection.fetch();
     },
     render: function() {
       var self = this;
 
-      var data = {user : this.userModel.toJSON()};
+      var data = {user : this.userModel.toJSON(), playlist : this.playlistCollection.toJSON()};
       var compiledTemplate = _.template( userTemplate, data );
 
       this.$el.html( compiledTemplate );
