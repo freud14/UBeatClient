511f90723a3ffa2c1665920b7b4bd64d5aecfb2c
Loic Vasseur <loic.vasseur.1@ulaval.ca>
2014-12-03 13:31:12 -0500

    Add Follow button and List of following on a user page, delete following works, add doesn't work

--- js/models/User.js
+++ js/models/User.js
@@ -10,7 +10,20 @@ define([
     fetch: function(options){
 	    options = _.defaults((options || {}), {url: config.API_URL + 'users/' + options.id});
       return Backbone.Model.prototype.fetch.call(this, options);
-    }
+    },
+
+    //Ne marche pas, utilisé pour follow un user
+    save: function(attributes, options) {
+      options = _.defaults((options || {}), {url: config.API_URL + 'follow'});
+      return Backbone.Model.prototype.save.call(this, attributes, options);
+    },
+
+    destroy: function(options) {
+      if(options.id){
+          options = _.defaults((options || {}), {url: config.API_URL + 'follow/' + options.id });
+      }
+      return Backbone.Model.prototype.destroy.call(this, options);
+    },
     
   });
   return User;
--- js/views/user.js
+++ js/views/user.js
@@ -4,9 +4,10 @@ define([
   'underscore',
   'backbone',
   'models/User',
+  'models/TokenInfo',
   'text!templates/user.html',
   'bootstrap-notify'
-], function($, _, Backbone, User, userTemplate){
+], function($, _, Backbone, User, TokenInfo, userTemplate){
   var UserView = Backbone.View.extend({
     el: $('#page-wrapper'),
     initialize: function(options) {
@@ -26,17 +27,61 @@ define([
       this.playlistCollection = new PlaylistCollection();
       this.playlistCollection.bind('change add remove sync', this.render, this);
       this.playlistCollection.fetch();
+
+      this.tokenInfoModel = new TokenInfo();
+      this.tokenInfoModel.fetch();
     },
+
     render: function() {
       var self = this;
 
-      var data = {user : this.userModel.toJSON(), playlist : this.playlistCollection.toJSON()};
+      var data = {user : this.userModel.toJSON(), playlist : this.playlistCollection.toJSON(), myId : this.tokenInfoModel.toJSON().id};
       var compiledTemplate = _.template( userTemplate, data );
 
       this.$el.html( compiledTemplate );
 
       return this;
     },
+
+    events: {
+      'click a#follow-button' : 'addFollow',
+      'click button.delete' : 'removeFollow',
+    },
+
+      //Ne marche pas, utilisé pour follow un user
+      addFollow: function(event) {
+          event.preventDefault();
+          event.stopImmediatePropagation();
+          followId = $(event.currentTarget).data('id');
+          console.log(followId);
+          var followingUser = new User({id : followId});
+          this.userModel.save({
+              data: JSON.stringify({ id: followId}),
+              type: "POST",
+              contentType: 'application/json'
+              },
+              {
+              success: function(data) {
+                  $('.top-center').notify({
+                      message: { text: 'Follow ajouté avec succès !' },
+                      fadeOut: { enabled: true, delay: 1000 }
+                  }).show();
+              }
+          });
+      },
+
+      removeFollow: function(event) {
+          followId = $(event.currentTarget).data('id');
+          this.userModel.destroy({id : followId}
+          ).done(function(){
+              $('.top-center').notify({
+                  message: { text: 'Follow supprimé' },
+                  fadeOut: { enabled: true, delay: 1000 },
+                  type: 'danger',
+              }).show();
+              $("#following-"+followId).hide();
+          });
+      },
   });
 
   return UserView;
